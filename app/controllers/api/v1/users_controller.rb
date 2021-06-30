d745723f38e2c76762830d85a1027ed10dc6107c
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-06-30 12:24:13 -0700

    working on auth

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,12 +1,18 @@
 class Api::V1::UsersController < ApplicationController
+    skip_before_action :authorized, only: [:create]
 
     def create
         user = User.new(user_params)
         if user.save
-            render json: { user: UserSerializer.new(user) }, status: :created
+            token = encode_token(user_id: user.id)
+            render json: { user: UserSerializer.new(user), jwt: token }, status: :created
         else
             render json: { error: 'failed to create user' }, status: :not_acceptable
         end
+    end
+
+    def profile
+        render json: { user: UserSerializer.new(current_user) }, status: :accepted
     end
 
     private
