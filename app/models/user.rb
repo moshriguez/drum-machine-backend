9f4cc5410af0210a20035665305d6381498b5eac
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-06-30 16:53:47 -0700

    more user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
     has_secure_password
 
     validates :username, presence: true, uniqueness: { case_sensitive: false }
+    validates :password, length: {minimum: 6, maximum: 20}
 end
