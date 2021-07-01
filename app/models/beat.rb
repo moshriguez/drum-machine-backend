888f3daff290510ee274b1cee656f47db8dc0197
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-07-01 10:48:17 -0700

    added dependent destroy

--- app/models/beat.rb
+++ app/models/beat.rb
@@ -1,10 +1,10 @@
 class Beat < ApplicationRecord
     belongs_to :user
     
-    has_many :comments
+    has_many :comments, dependent: :destroy
     has_many :users, through: :comments
 
-    has_many :beat_pads
+    has_many :beat_pads, dependent: :destroy
     has_many :pads, through: :beat_pads
 
     validates :name, presence: true, uniqueness: { scope: :user, case_sensitive: false }
