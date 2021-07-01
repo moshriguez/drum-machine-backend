bbb57e14a8362deb051b50ea6d52ea5e7ccb8409
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-06-30 23:03:27 -0700

    drew basic routes for models

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:create]
+      resources :users, only: [:create, :update]
+      resources :beats, only: [:create, :update]
+      resources :comments, only: [:create]
+      resources :beat_pads
+      resources :pads
       post '/login', to: 'auth#create'
       get '/profile', to: 'users#profile'
     end
