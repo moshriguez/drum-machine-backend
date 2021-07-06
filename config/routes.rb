797872d7847db55700a5ecdffa46b7c35dac3bc5
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-07-06 15:33:48 -0700

    added show route for beats

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:index, :create, :update, :destroy]
-      resources :beats, only: [:index, :create, :update]
+      resources :beats, only: [:index, :show, :create, :update]
       resources :comments, only: [:create]
       resources :beat_pads
       resources :pads
