898c53485bff70e5f3f4706fa59456553abea376
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-07-06 13:20:37 -0700

    added dependent destroy for beats and added an if statement to the password validation so update can work

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
     has_secure_password
-    has_many :beats
+    has_many :beats, dependent: :destroy
 
     has_many :comments
     has_many :commented_beats, through: :comments
 
     validates :username, presence: true, uniqueness: { case_sensitive: false }
-    validates :password, length: {minimum: 6, maximum: 20}
+    validates :password, length: {minimum: 6, maximum: 20}, if: -> {new_record? || !password.nil?}
 end
