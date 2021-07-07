6262673206a8531ccd6e9f716b0d1e7a10058227
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-07-07 12:31:23 -0700

    updated serializers with custom methods

--- app/serializers/beat_serializer.rb
+++ app/serializers/beat_serializer.rb
@@ -1,7 +1,11 @@
 class BeatSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :tempo
+  attributes :id, :name, :description, :tempo, :user_id
   belongs_to :user
   
   has_many :comments
   has_many :beat_pads
+
+  attribute :username do
+    self.object.user.username
+  end
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,6 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :username, :bio
   has_many :beats
+  has_many :comments
+  has_many :commented_beats, serializer: BeatSerializer
 end
