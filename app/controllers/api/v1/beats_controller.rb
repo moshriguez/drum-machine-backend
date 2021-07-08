a4a6f2e5976f533306730b19318b196d177699ea
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-07-08 16:21:46 -0700

    10 pads now working

--- app/controllers/api/v1/beats_controller.rb
+++ app/controllers/api/v1/beats_controller.rb
@@ -20,15 +20,27 @@ class Api::V1::BeatsController < ApplicationController
 
     def create
         beat = Beat.new(beat_params)
-        pad1 = Pad.find(1)
-        pad2 = Pad.find(2)
-        pad3 = Pad.find(3)
-        pad4 = Pad.find(4)
+        pad1 = Pad.find(5)
+        pad2 = Pad.find(6)
+        pad3 = Pad.find(7)
+        pad4 = Pad.find(8)
+        pad5 = Pad.find(9)
+        pad6 = Pad.find(10)
+        pad7 = Pad.find(11)
+        pad8 = Pad.find(12)
+        pad9 = Pad.find(13)
+        pad10 = Pad.find(14)
         if beat.save
             BeatPad.create(volume: params[:pad1][:volume], sequence: params[:pad1][:sequence], beat_id: beat.id, pad_id: pad1.id)
             BeatPad.create(volume: params[:pad2][:volume], sequence: params[:pad2][:sequence], beat_id: beat.id, pad_id: pad2.id)
             BeatPad.create(volume: params[:pad3][:volume], sequence: params[:pad3][:sequence], beat_id: beat.id, pad_id: pad3.id)
             BeatPad.create(volume: params[:pad4][:volume], sequence: params[:pad4][:sequence], beat_id: beat.id, pad_id: pad4.id)
+            BeatPad.create(volume: params[:pad5][:volume], sequence: params[:pad5][:sequence], beat_id: beat.id, pad_id: pad5.id)
+            BeatPad.create(volume: params[:pad6][:volume], sequence: params[:pad6][:sequence], beat_id: beat.id, pad_id: pad6.id)
+            BeatPad.create(volume: params[:pad7][:volume], sequence: params[:pad7][:sequence], beat_id: beat.id, pad_id: pad7.id)
+            BeatPad.create(volume: params[:pad8][:volume], sequence: params[:pad8][:sequence], beat_id: beat.id, pad_id: pad8.id)
+            BeatPad.create(volume: params[:pad9][:volume], sequence: params[:pad9][:sequence], beat_id: beat.id, pad_id: pad9.id)
+            BeatPad.create(volume: params[:pad10][:volume], sequence: params[:pad10][:sequence], beat_id: beat.id, pad_id: pad10.id)
             render json: {beat: BeatSerializer.new(beat), message: 'Your beat was saved successfully.'}, status: :created
         else
             render json: {error: beat.errors.full_messages}, status: :unprocessable_entity
@@ -37,15 +49,27 @@ class Api::V1::BeatsController < ApplicationController
 
     def update
         beat = Beat.find(params[:id])
-        pad1 = beat.beat_pads.find{|bp| bp.pad_id == 1}
-        pad2 = beat.beat_pads.find{|bp| bp.pad_id == 2}
-        pad3 = beat.beat_pads.find{|bp| bp.pad_id == 3}
-        pad4 = beat.beat_pads.find{|bp| bp.pad_id == 4}
+        pad1 = beat.beat_pads.find{|bp| bp.pad_id == 5}
+        pad2 = beat.beat_pads.find{|bp| bp.pad_id == 6}
+        pad3 = beat.beat_pads.find{|bp| bp.pad_id == 7}
+        pad4 = beat.beat_pads.find{|bp| bp.pad_id == 8}
+        pad5 = beat.beat_pads.find{|bp| bp.pad_id == 9}
+        pad6 = beat.beat_pads.find{|bp| bp.pad_id == 10}
+        pad7 = beat.beat_pads.find{|bp| bp.pad_id == 11}
+        pad8 = beat.beat_pads.find{|bp| bp.pad_id == 12}
+        pad9 = beat.beat_pads.find{|bp| bp.pad_id == 13}
+        pad10 = beat.beat_pads.find{|bp| bp.pad_id == 14}
         if beat.update(beat_params)
             pad1.update(volume: params[:pad1][:volume], sequence: params[:pad1][:sequence])
             pad2.update(volume: params[:pad2][:volume], sequence: params[:pad2][:sequence])
             pad3.update(volume: params[:pad3][:volume], sequence: params[:pad3][:sequence])
             pad4.update(volume: params[:pad4][:volume], sequence: params[:pad4][:sequence])
+            pad5.update(volume: params[:pad5][:volume], sequence: params[:pad5][:sequence])
+            pad6.update(volume: params[:pad6][:volume], sequence: params[:pad6][:sequence])
+            pad7.update(volume: params[:pad7][:volume], sequence: params[:pad7][:sequence])
+            pad8.update(volume: params[:pad8][:volume], sequence: params[:pad8][:sequence])
+            pad9.update(volume: params[:pad9][:volume], sequence: params[:pad9][:sequence])
+            pad10.update(volume: params[:pad10][:volume], sequence: params[:pad10][:sequence])
             render json: {beat: BeatSerializer.new(beat), message: 'Your beat was saved successfully.'}, status: :created
         else
             render json: {error: beat.errors.full_messages}, status: :unprocessable_entity
@@ -54,6 +78,6 @@ class Api::V1::BeatsController < ApplicationController
 
     private
     def beat_params
-        params.permit(:name, :description, :user_id, :tempo, :pad1, :pad2, :pad3, :pad4)
+        params.permit(:name, :description, :user_id, :tempo, :pad1, :pad2, :pad3, :pad4, :pad5, :pad6, :pad7, :pad8, :pad9, :pad10)
     end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,7 @@ puts 'Destroying old data...'
 Beat.destroy_all
 Comment.destroy_all
 BeatPad.destroy_all
+Pad.destroy_all
 
 # puts 'Creating users...'
 
@@ -15,22 +16,42 @@ BeatPad.destroy_all
 
 puts 'Creating beats...'
 
-kick = Pad.find(1)
-snare = Pad.find(2)
-hi_hats = Pad.find(3)
-open_hi_hats = Pad.find(4)
-# kick = Pad.create(name: 'kick', sample_file: 'kick.wav')
-# snare = Pad.create(name: 'snare', sample_file: 'snare.wav')
-# hi_hats = Pad.create(name: 'hi-hats', sample_file: 'hi-hats.wav')
-# open_hi_hats = Pad.create(name: 'open hi-hats', sample_file: 'open hi-hats.wav')
+kick1 = Pad.find(5)
+kick2 = Pad.find(6)
+snare = Pad.find(7)
+clap = Pad.find(8)
+rim = Pad.find(9)
+hh1 = Pad.find(10)
+hh2 = Pad.find(11)
+hh_open = Pad.find(12)
+ride = Pad.find(13)
+shaker = Pad.find(14)
+
+# In case you need to create the pads again:
+# kick1 = Pad.create(name: 'Kick1', sample_file: 'SB15_Drm_bd.wav')
+# kick2 = Pad.create(name: 'Kick2', sample_file: 'kick13.wav')
+# snare = Pad.create(name: 'Snare', sample_file: 'tracks_15 #006.wav')
+# clap = Pad.create(name: 'Clap', sample_file: 'lil j clap 6.wav')
+# rim = Pad.create(name: 'Rim', sample_file: 'mobb-snare1-qbh.wav')
+# hh1 = Pad.create(name: 'HH1', sample_file: 'tracks_19 #004.wav')
+# hh2 = Pad.create(name: 'HH2', sample_file: 'tight ass hat 3.wav')
+# hh_open = Pad.create(name: 'HH Open', sample_file: 'tracks_61 #008.wav')
+# ride = Pad.create(name: 'Ride', sample_file: 'c22 CshRd R02   x.wav')
+# shaker = Pad.create(name: 'Shaker', sample_file: 'JBlaze_shaker5.wav')
 
 20.times do
     beat = Beat.create(name: Faker::Ancient.god, description: Faker::Books::Lovecraft.sentence, tempo: Random.new.rand(40..280), user_id: User.all.sample.id)
 
-    BeatPad.create(beat_id: beat.id, pad: kick, volume: Random.new.rand(0..1.5).round(2), sequence: Random.new.rand(32768..65535).to_s(2))
+    BeatPad.create(beat_id: beat.id, pad: kick1, volume: Random.new.rand(0..1.5).round(2), sequence: Random.new.rand(32768..65535).to_s(2))
+    BeatPad.create(beat_id: beat.id, pad: kick2, volume: Random.new.rand(0..1.5).round(2), sequence: Random.new.rand(32768..65535).to_s(2))
     BeatPad.create(beat_id: beat.id, pad: snare, volume: Random.new.rand(0..1.5).round(2), sequence: Random.new.rand(32768..65535).to_s(2))
-    BeatPad.create(beat_id: beat.id, pad: hi_hats, volume: Random.new.rand(0..1.5).round(2), sequence: Random.new.rand(32768..65535).to_s(2))
-    BeatPad.create(beat_id: beat.id, pad: open_hi_hats, volume: Random.new.rand(0..1.5).round(2), sequence: Random.new.rand(32768..65535).to_s(2))
+    BeatPad.create(beat_id: beat.id, pad: clap, volume: Random.new.rand(0..1.5).round(2), sequence: Random.new.rand(32768..65535).to_s(2))
+    BeatPad.create(beat_id: beat.id, pad: rim, volume: Random.new.rand(0..1.5).round(2), sequence: Random.new.rand(32768..65535).to_s(2))
+    BeatPad.create(beat_id: beat.id, pad: hh1, volume: Random.new.rand(0..1.5).round(2), sequence: Random.new.rand(32768..65535).to_s(2))
+    BeatPad.create(beat_id: beat.id, pad: hh2, volume: Random.new.rand(0..1.5).round(2), sequence: Random.new.rand(32768..65535).to_s(2))
+    BeatPad.create(beat_id: beat.id, pad: hh_open, volume: Random.new.rand(0..1.5).round(2), sequence: Random.new.rand(32768..65535).to_s(2))
+    BeatPad.create(beat_id: beat.id, pad: ride, volume: Random.new.rand(0..1.5).round(2), sequence: Random.new.rand(32768..65535).to_s(2))
+    BeatPad.create(beat_id: beat.id, pad: shaker, volume: Random.new.rand(0..1.5).round(2), sequence: Random.new.rand(32768..65535).to_s(2))
     
     puts 'creating comments...'
     3.times do
