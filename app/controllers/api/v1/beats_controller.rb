60fdbd6f8ccb5448815bc600060f7299a40ac67a
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-07-07 19:31:47 -0700

    added create action to beats; it works

--- app/controllers/api/v1/beats_controller.rb
+++ app/controllers/api/v1/beats_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::BeatsController < ApplicationController
-    skip_before_action :authorized, only: [:show]
+    skip_before_action :authorized, only: [:show, :create]
 
     def index
         beats = Beat.all
@@ -20,15 +20,27 @@ class Api::V1::BeatsController < ApplicationController
 
     def create
         beat = Beat.new(beat_params)
+        pad1 = Pad.find(1)
+        pad2 = Pad.find(2)
+        pad3 = Pad.find(3)
+        pad4 = Pad.find(4)
         if beat.save
-            render json: {beat: BeatSerializer.new(beat)}, status: :created
+            BeatPad.create(volume: params[:pad1][:volume], sequence: params[:pad1][:sequence], beat_id: beat.id, pad_id: pad1.id)
+            BeatPad.create(volume: params[:pad2][:volume], sequence: params[:pad2][:sequence], beat_id: beat.id, pad_id: pad2.id)
+            BeatPad.create(volume: params[:pad3][:volume], sequence: params[:pad3][:sequence], beat_id: beat.id, pad_id: pad3.id)
+            BeatPad.create(volume: params[:pad4][:volume], sequence: params[:pad4][:sequence], beat_id: beat.id, pad_id: pad4.id)
+            render json: {beat: BeatSerializer.new(beat), message: 'Your beat was saved successfully.'}, status: :created
         else
             render json: {error: beat.errors.full_messages}, status: :unprocessable_entity
         end
     end
 
+    def update
+        
+    end
+
     private
     def beat_params
-        params.permit(:name, :description, :user_id)
+        params.permit(:name, :description, :user_id, :tempo, :pad1, :pad2, :pad3, :pad4)
     end
 end
