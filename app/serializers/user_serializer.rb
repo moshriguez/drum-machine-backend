a59d0ec75466018bfae60400459f24c425f2fa81
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-07-10 14:52:52 -0700

    added musical influences to serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :bio
+  attributes :id, :username, :bio, :musical_influences
   has_many :beats
   has_many :comments
   has_many :commented_beats, serializer: BeatSerializer
