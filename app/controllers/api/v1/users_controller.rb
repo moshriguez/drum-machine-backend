3f59abcf6763e02a97b07ae35fa4a9f6ccd60a9e
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-07-13 10:13:49 -0700

    added musical influences to user params

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -45,6 +45,6 @@ class Api::V1::UsersController < ApplicationController
 
     private
     def user_params
-        params.permit(:username, :password, :bio)
+        params.permit(:username, :password, :bio, :musical_influences)
     end
 end
